728240665de9dccbc7433801767f08b34bcd601d
Ethan Overland <overlandethan@gmail.com>
2024-11-17 14:22:49 -0900

    Building initial controller with basic get method. No employee model yet.

---
+++ eo_EmployeeMangement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eo_EmployeeMangement.API.Controllers
+{
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public JsonResult GetEmployees()
+        {
+            return new JsonResult(
+                new List<object> {
+                    new { id = 1, Name = "John Doe"},
+                    new { id = 2, Name = "Jane Doe"}
+                });
+        }
+    }
+}
