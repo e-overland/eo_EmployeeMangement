7144bbe783d0fe7eefb8b053b7949b2a5b99e36b
Ethan Overland <overlandethan@gmail.com>
2024-11-17 14:49:30 -0900

    Added Employees basic model.

---
+++ eo_EmployeeMangement.API/Models/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace eo_EmployeeMangement.API.Models
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Position { get; set; }
+        public DateOnly StartDate { get; set; }
+
+    }
+}
